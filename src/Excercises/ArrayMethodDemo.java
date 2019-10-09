4dfda63daa18c0cbafb4666253f0cf7388f98766
DakotaWolfe <54542632+DakotaWolfe@users.noreply.github.com>
2019-10-09 08:59:44 -0500

    commit

---
+++ src/Excercises/ArrayMethodDemo.java
@@ -0,0 +1,9 @@
+package Excercises;
+
+public class ArrayMethodDemo {
+
+    public static void main(String[] args){
+
+
+    }
+}
--- src/Excercises/DistanceFromAverage.java
+++ src/Excercises/DistanceFromAverage.java
@@ -1,8 +1,45 @@
 package Excercises;
 
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
 public class DistanceFromAverage {
 
     public static void main(String[] args){
+        double[] arryNums = new double[20];
+        boolean quit = true;
+        int count = 0;
+        double userEntry;
+        double total = 0;
+        double average = 0;
+        double distance = 0;
+        Scanner input = new Scanner(System.in);
 
+        while(quit){
+
+            System.out.println("Please enter a number >>> ");
+            userEntry = input.nextDouble();
+            count++;
+            if (count == 20){
+                quit = false;
+            }
+            else if(userEntry == 99999){
+                quit = false;
+            }
+            else{
+                arryNums[count] = userEntry;
+                total = total + userEntry;
+                count++;
+            }
+
+        }
+        average = total/count;
+
+        for(int i = 0; i < count; i++){
+            distance = arryNums[i] - average;
+            System.out.println("The average was " + average + " \nFirst entry was " + arryNums[i] +
+            "\n The distance was " + abs(distance) + "\n Total Count " + count);
+        }
     }
 }
